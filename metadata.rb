43a76eb74303ed4ddfd80d9bda733928cab6d379
Fletcher Nichol <fnichol@nichol.ca>
2010-11-28 19:41:11 -0700

    Add official_repository recipe.

--- metadata.rb
+++ metadata.rb
@@ -4,3 +4,12 @@ license          "Apache 2.0"
 description      "Installs/Configures NVIDIA graphics drivers on Ubuntu Linux"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.1"
+
+recipe "ubuntu_nvidia_gpu",     "Default, empty recipe"
+recipe "ubuntu_nvidia_gpu::official_repository",  
+  "Installs NVIDIA's graphics drivers with Ubuntu's official packages"
+
+%w{ ubuntu }.each do |os|
+  supports os
+end
+
