223ed7ad0cfc308d2ddde0a48a37c074f53eec51
Fletcher Nichol <fnichol@nichol.ca>
2012-10-07 22:13:23 -0600

    Reformat metadata.rb.

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,9 @@
-maintainer       "Fletcher Nichol"
-maintainer_email "fnichol@nichol.ca"
-license          "Apache 2.0"
-description      "Installs/Configures NVIDIA graphics drivers on Ubuntu Linux"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+name              "ubuntu_nvidia_gpu"
+maintainer        "Fletcher Nichol"
+maintainer_email  "fnichol@nichol.ca"
+license           "Apache 2.0"
+description       "Installs/Configures NVIDIA graphics drivers on Ubuntu Linux"
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version           "0.1.0"
 
 supports "ubuntu"
