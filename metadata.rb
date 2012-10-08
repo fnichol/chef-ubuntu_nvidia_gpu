92fd3e4fb55e3446ded0b26962119c411ebf12c5
Fletcher Nichol <fnichol@nichol.ca>
2012-10-07 21:18:43 -0600

    The Grand Update.

--- metadata.rb
+++ metadata.rb
@@ -2,14 +2,7 @@ maintainer       "Fletcher Nichol"
 maintainer_email "fnichol@nichol.ca"
 license          "Apache 2.0"
 description      "Installs/Configures NVIDIA graphics drivers on Ubuntu Linux"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.1"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
 
-recipe "ubuntu_nvidia_gpu",     "Default, empty recipe"
-recipe "ubuntu_nvidia_gpu::official_repository",  
-  "Installs NVIDIA's graphics drivers with Ubuntu's official packages"
-
-%w{ ubuntu }.each do |os|
-  supports os
-end
-
+supports "ubuntu"
